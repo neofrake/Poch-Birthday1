3e3e8b2e9ac7062d71abf04df867358610246533
Sokseyha Kruy <kruysokseyha@gmail.com>
2024-08-28 22:13:48 +0700

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,16 @@
+const title = document.querySelector(".title");
+const text = "Hello, i have a surprise for you! ".split("");
+
+for (let index = 0; index < text.length; index++) {
+  if (text[index] !== " ") {
+    title.innerHTML += `<span>${text[index]}<span/>`;
+  } else {
+    title.innerHTML += `<span style='margin-right: 20px;'><span/>`;
+  }
+}
+
+const textElements = document.querySelectorAll(".title span");
+textElements.forEach((element) => {
+  const randomDelay = Math.random() * 0.5;
+  element.style.animationDelay = `${randomDelay}s`;
+});
